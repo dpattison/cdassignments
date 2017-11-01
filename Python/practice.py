50f74f5105869a220ef9060f103ebbbbbbdef623
Dave Pattison <dpattison@missionlinen.com>
2017-10-31 20:59:52 -0700

    practice

--- Python/practice.py
+++ Python/practice.py
@@ -1,3 +1,20 @@
+"""
 words = "It's thanksgiving day. It's my birthday,too!"
 find = "day"
-words.find(find)
+print words.find(find)
+month = "month"
+print words.replace(find, month)
+x = [2, 54, -2, 7, 12, 98]
+print min(x)
+print max(x)
+
+x = ["hello", 2, 54, -2, 7, 12, 98, "world"]
+print x[0]
+print x[len(x) - 1]
+y = [x[0], x[len(x) - 1]]
+print y
+"""
+
+x = [19, 2, 54, -2, 7, 12, 98, 32, 10, -3, 6]
+x.sort()
+print x
