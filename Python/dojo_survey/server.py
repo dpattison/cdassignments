4f57122cc9cdb5acef5980aff8f51690b3af515a
pinsoftware <8990877+dpattison@users.noreply.github.com>
2017-11-12 12:54:32 -0800

    start of dojo survey

---
+++ Python/dojo_survey/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/process', methods=['POST'])
+def create_user():
+    name = request.form['name']
+    location = request.form['location']
+    language = request.form['language']
+    comment = request.form['comment']
+    return render_template("result.html", name=name, location=location, language=language, comment=comment)
+
+
+app.run(debug=True)
