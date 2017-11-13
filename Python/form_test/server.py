3a7136e5405eacc7bc93226a162d5ab25249ddd0
pinsoftware <8990877+dpattison@users.noreply.github.com>
2017-11-12 20:24:25 -0800

    formtest

--- Python/form_test/server.py
+++ Python/form_test/server.py
@@ -14,5 +14,10 @@ def create_user():
     name = request.form['name']
     email = request.form['email']
 
+@app.route('/show')
+def show_user():
+  return render_template('user.html', name='Jay', email='kpatel@codingdojo.com')
+
+
 
 app.run(debug=True)
