6da1637f7b502af2b2380e83b4fe3f76ac40b5a1
Dave Pattison <dpattison@missionlinen.com>
2017-12-21 17:35:39 -0800

    courses - models

---
+++ Python/coursesproj/courses/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from courses.models import *
+
+
+def index(request):
+    return render(request, 'courses/index.html', { "courses": Course.objects.all() })
+
+def create(request):
+    new_course = Course.objects.create(name=request.POST['name'], description=request.POST['description'])
+    id = new_course.id
+    return redirect('index')
+
+def destroy(request, id):
+    course = Course.objects.get(id = id)
+    course.delete()
+    return redirect('index')
